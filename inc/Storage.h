e2201b2e9624c23bd7755d3ccfb3c2f2cfc2539f
Bradley Dufour <dufour.bradley@gmail.com>
2025-05-13 14:01:09 -0400

    Add storage class for powders

---
+++ inc/Storage.h
@@ -0,0 +1,42 @@
+#ifndef STORAGE_H
+#define STORAGE_H
+
+#include "IPowder.h"
+
+class Storage {
+    private:
+        /**
+         * List of all present powders
+         */
+        std::vector<Powder::IPowder*>* powders;
+        /**
+         * 2-d map for tracking powder locations
+         */
+        std::unordered_map<int, std::unordered_map<int, Powder::IPowder*>*>* powderLocs;
+
+    public:
+        Storage();
+        ~Storage();
+        
+        /**
+         * Add a powder to the simulation
+         * 
+         * @return True if powder successfully added, else false
+         */
+        bool addPowder(Powder::IPowder* toAdd);
+
+        /**
+         * Add a powder to the locations map, adding new indices to the map as needed
+         * 
+         * @return True if powder successfully added, else false
+         */
+        bool addToLocations(Powder::IPowder* toAdd);
+
+        /**
+         * Remove a powder from the locations map
+         * 
+         * @return A pointer to the removed Powder
+         */
+        Powder::IPowder* removeFromLocations(Powder::IPowder* toRemove);
+};
+#endif /* APP_HPP */
---
+++ src/Storage.cpp
@@ -0,0 +1,35 @@
+#include "Storage.h"
+
+Storage::Storage() {
+    powders = new std::vector<Powder::IPowder*>();
+    powderLocs = new std::unordered_map<int, std::unordered_map<int, Powder::IPowder*>*>();
+}
+
+Storage::~Storage() {
+    delete powders;
+    powderLocs->clear();
+    delete powderLocs;
+}
+
+bool Storage::addPowder(Powder::IPowder* toAdd) {
+    bool safeToAdd = addToLocations(toAdd);
+    if(!safeToAdd) {
+        printf("WARNING: Tried to add powder where there was already one\n");
+    } else {
+        powders->push_back(toAdd);
+    }
+    return safeToAdd;
+}
+
+bool Storage::addToLocations(Powder::IPowder* toAdd) {
+    int* pos = toAdd->getPosition();
+    
+    powderLocs->emplace(std::make_pair(pos[0], new std::unordered_map<int, Powder::IPowder*>()));
+    return(std::get<bool>(powderLocs->at(pos[0])->emplace(pos[1], toAdd)));
+}
+
+Powder::IPowder* Storage::removeFromLocations(Powder::IPowder* toRemove) {
+    int* curPos = toRemove->getPosition();
+    powderLocs->at(curPos[0])->erase(curPos[1]);
+    return(toRemove);
+}
